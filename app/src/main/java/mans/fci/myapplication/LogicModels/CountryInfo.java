db2bf87055eb1667507ffc2f32be1f474da82b68
waleed m <waleed.m.m@gmail.com>
2020-10-23 21:33:06 +0200

    v5-Adding 2 classes to store MedicineInfo and CountryInfo

---
+++ app/src/main/java/mans/fci/myapplication/LogicModels/CountryInfo.java
@@ -0,0 +1,6 @@
+package mans.fci.myapplication.LogicModels;
+
+public class CountryInfo {
+    public int country_id;
+    public  String country_title;
+}
---
+++ app/src/main/java/mans/fci/myapplication/LogicModels/MedicineInfo.java
@@ -0,0 +1,15 @@
+package mans.fci.myapplication.LogicModels;
+
+public class MedicineInfo {
+    public int Id;
+    public int country_id;
+    public String title;
+    public int[] form;
+    public String description;
+    public String producent;
+    public String active_ingredient;
+    public int category_id;
+    public int compatibility_id;
+    public int ingredient_group_id;
+    public boolean is_favorite;
+}
